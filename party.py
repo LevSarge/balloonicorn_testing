63f048bced96ab3026fdaae6feba05a1445a30e0
LevSarge <levimsargeant@gmail.com>
2022-08-11 18:26:49 -0600

    Added tests to most_and_least_common_types function

--- party.py
+++ party.py
@@ -26,6 +26,18 @@ def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
 
     Return most and least common treat types in tuple of format (most, least).
+
+    >>> treats = [{'type': 'dessert'}, {'type': 'dessert'}, {'type': 'drink'}]
+    >>> most_and_least_common_type(treats)
+    ('dessert', 'drink')
+
+    >>> treats = [{'type': 'brownies'}, {'type': 'brownies'}]
+    >>> most_and_least_common_type(treats)
+    ('brownies', 'brownies')
+
+    >>> treats = []
+    >>> most_and_least_common_type(treats)
+    (None, None)
     """
 
     types = {}
